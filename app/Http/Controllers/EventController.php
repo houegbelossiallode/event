035a506261f063160847db6b1f131929f225426b
Goldfroy <houegbelossiallode@gmail.com>
2024-09-17 22:09:57 +0100

    update configuration

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,29 +12,15 @@ class EventController extends Controller
 {
     public function index()
 {
-        Event::create([
-            'name'=> 'Concert de Jazz',
-            'description'=>'Un concert de jazz avec des artistes locaux.',
-            'date'=> '2024-10-01',
-            'price'=> '15000.00',
-            'available_tickets'=> '100'
-        ]);
+       // Event::create([
+       //     'name'=> 'Concert de Jazz',
+       //     'description'=>'Un concert de jazz avec des artistes locaux.',
+       //     'date'=> '2024-10-01',
+       //     'price'=> '15000.00',
+       //     'available_tickets'=> '100'
+      //  ]);
 
-        Event::create([
-            'name'=> 'Conférence Tech',
-            'description'=>'Une conférence sur les dernières innovations techn...',
-            'date'=> '2024-09-15',
-            'price'=> '50.00',
-            'available_tickets'=> '100'
-        ]);
-
-         Event::create([
-            'name'=> 'Concert Welove Eya',
-            'description'=>'Un concert pour enjailler les vacances.',
-            'date'=> '2025-04-03',
-            'price'=> '100.00',
-            'available_tickets'=> '100'
-        ]);
+       
     $events = Event::all(); // Récupérer tous les événements
     return view('events.index', compact('events'));
 }
